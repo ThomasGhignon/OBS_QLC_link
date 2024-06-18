75d37356efcbb15a4164e9e9ff4af75e52d9e126
thomasghignon <ghignon.thomas@gmail.com>
2024-06-18 13:40:02 +0200

    ✨update obs ip to localhost

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import '../style.css'
 import ObsController from "./controller/obs_controller.js";
 import QlcController from "./controller/qlc_controller.js";
 
-const obsWebSocket = new ObsController('172.28.59.33', '4455')
+const obsWebSocket = new ObsController('localhost', '4455')
 const qlcWebSocket = new QlcController('ws://localhost:9999/qlcplusWS');
